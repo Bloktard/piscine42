8744d40d8f28bd5b2001303734baf5c93331f729
Paul Lerick <plerick@c1r4p7.42lehavre.fr>
2024-08-12 22:13:32 +0200

    c01 ex04 done

--- C 01/ex03/ft_div_mod.c
+++ C 01/ex03/ft_div_mod.c
@@ -6,16 +6,33 @@
 /*   By: plerick <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 19:25:22 by plerick           #+#    #+#             */
-/*   Updated: 2024/08/11 19:31:47 by plerick          ###   ########.fr       */
+/*   Updated: 2024/08/12 14:48:59 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_div_mod(int a, int b, int *div, int *mod)
-{
-	q
-}
+#include <unistd.h>
+#include <stdio.h>
 
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+/*
 int	main(void)
 {
+	int	a;
+	int	b;
+	int	resu;
+	int	rest;
+
+	a = 26;
+	b = 5;
+	resu = 55;
+	rest = 55;
+	ft_div_mod(a, b, &resu, &rest);
+	printf("26 / 5 = %d", resu);
+	printf(" reste = %d", rest);
 	return (0);
 }
+*/
--- C 01/ex04/ft_ultimate_div_mod.c
+++ C 01/ex04/ft_ultimate_div_mod.c
@@ -1,28 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plerick <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/10 13:59:58 by plerick           #+#    #+#             */
-/*   Updated: 2024/08/12 13:47:06 by plerick          ###   ########.fr       */
+/*   Created: 2024/08/12 14:49:36 by plerick           #+#    #+#             */
+/*   Updated: 2024/08/12 21:43:31 by plerick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-void	ft_ft(int *nbr)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*nbr = 42;
+	int	tempo1;
+	int	tempo2;
+
+	tempo1 = *a / *b;
+	tempo2 = *a % *b;
+	*a = tempo1;
+	*b = tempo2;
 }
 /*
 int	main(void)
 {
-	int	i;
+	int	a;
+	int	b;
+	int	resu;
+	int	rest;
 
-	ft_ft(&i);
-	write(1, &i, 2);
+	a = 26;
+	b = 5;
+	resu = a;
+	rest = b;
+	ft_ultimate_div_mod(&resu, &rest);
+	printf("26 / 5 = %d", resu);
+	printf(" reste = %d", rest);
 	return (0);
 }
 */
